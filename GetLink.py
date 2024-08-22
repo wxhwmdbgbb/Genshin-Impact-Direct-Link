8ccde2f01608147fab42b2c2893f6a087ef4fdff
wxhwmdbgbb <wxhwmdbgbb@gmail.com>
2024-08-22 13:40:55 +0800

    add code to filter game version

--- GetLink.py
+++ GetLink.py
@@ -1,21 +1,36 @@
-import requests,json,jsonpath
+import requests,jsonpath,os
 
-
-# 存储文本
-def data_save(filename, data):
+# 存储链接文本
+def save_links(filename, data, version):
+    # 分割字符串，使每个连接单独成行
+    data = str(data).replace(',',',\n')
+    data = data.replace('[','')
+    data = data.replace(']','')
+    data = data.replace("'",'')
+    tempfile = open('tempfile.txt','w')
+    tempfile.write(data)
+    tempfile.close()
+    tempfile = open('tempfile.txt','r')
+    tempdata = tempfile.readlines()
     file = open(filename,'w')
-    file.write(data)
+    datalink = 0
+    for line in tempdata:
+        if version in line:
+            file.write(line)
+            datalink = datalink + 1
+            # print(line)
+    if datalink == 0:
+        print("There is no " + version + " version!!!")
+        file.write("There is no " + version + " version!!!")
+    tempfile.close()
     file.close()
-    print("The file was saved successfully !")
+    # 删除临时文本
+    os.remove('./tempfile.txt')
+    print("The file was saved successfully! Please review link.txt documents.")
 
 res = requests.get('https://sg-hyp-api.hoyoverse.com/hyp/hyp-connect/api/getGamePackages?launcher_id=VYTpXlbWo8')
 # 提取下载连接
-data = jsonpath.JSONPath("$.data.game_packages..main..game_pkgs..url").parse(res.json())
-
-# 分割字符串，使每个连接单独成行
-data = str(data).replace(',',',\n')
-data = data.replace('[','')
-data = data.replace(']','')
-data = data.replace("'",'')
+data = jsonpath.JSONPath("$..game_packages..url").parse(res.json())
+version = input("Please input game version:")
 # save file data
-data_save('link.txt',str(data))
+save_links('link.txt',str(data),version)
