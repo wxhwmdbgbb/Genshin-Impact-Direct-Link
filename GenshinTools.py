ac32a9b963965b52b4d81582b00c3b97a5de073e
wxhwmdbgbb <wxhwmdbgbb@gmail.com>
2024-08-24 23:35:47 +0800

    Optimize the text box code

--- GenshinTools.py
+++ GenshinTools.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from tkinter import messagebox
 import requests, os, jsonpath
 from tkinter import Text
+from tkinter import Tk
 
 url = "https://sg-hyp-api.hoyoverse.com/hyp/hyp-connect/api/getGamePackages?launcher_id=VYTpXlbWo8"
 
 
-class GenshinApplication(Frame):
+class GenshinDirectLink(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -14,7 +14,7 @@ class GenshinApplication(Frame):
         self.createWidget()
         self.resok = False
         self.showtext = Text(self.master, autoseparators=False, exportselection=True)
-        self.showtext.place(anchor="center", width=800, height=480, relx=0.5, rely=0.5)
+        self.master.bind("<Configure>", self.onresize)
 
     def createWidget(self):
 
@@ -63,18 +63,54 @@ class GenshinApplication(Frame):
                 output.close()
 
         else:
-            output.write("error")
+            output.write("Error!!!Please enter the game version!")
             output.close()
         output = open("./output.txt", "r")
         self.showtext.delete("1.0", END)
         self.showtext.insert("insert", output.read())
-        print(output.read())
         output.close()
+
+    def onresize(self, event=None):
+        if event:
+            self.showtext_width = self.master.winfo_width()
+            self.showtext_height = self.master.winfo_height()
+            self.showtext.place(
+                anchor="center",
+                width=self.showtext_width,
+                height=self.showtext_height,
+                relx=0.5,
+                rely=0.6,
+            )
+
+
+class mainwindow(Tk):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("GenshinTools")
+        self.width = int(self.winfo_screenwidth() / 2)
+        self.height = int(self.winfo_screenheight() / 2)
+        window_size = "{}x{}".format(self.width, self.height)
+        self.geometry(window_size)
+        self.maxsize(self.winfo_screenwidth(), self.winfo_screenheight())
+        self.minsize(self.width, self.height)
+        self.bind("<Configure>", self.windowresize)
+
+    def windowresize(self, event=None):
+        if event:
+            self.width = self.winfo_width()
+            self.height = self.winfo_height()
 
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title("GenshinTools")
-    root.geometry("800x600")
-    app = GenshinApplication(root)
+    # root = Tk()
+    # root.title("GenshinTools")
+
+    # root.geometry("800x600")
+    # root.minsize(800, 600)
+    # root.maxsize(1024, 600)
+    # print(root.winfo_width())
+    # print(root.winfo_height())
+    root = mainwindow()
+    app = GenshinDirectLink(root)
+
     root.mainloop()
